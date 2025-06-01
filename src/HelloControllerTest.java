042636d1992aa599a9a2ccc8143186ca4a45be93
spineless69 <chessloidforger@gmail.com>
2025-06-01 22:16:58 +0530

    Create HelloControllerTest.java

---
+++ src/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.shopping.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloEndpointReturnsExpectedResponse() throws Exception {
+        mockMvc.perform(get("/hello"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("Hello from Shopping Cart!"));
+    }
+}
